a43dc60477935a37764fe063dc03e426785035dd
rystei <gustavo.telles@edu.unifil.br>
2024-02-22 13:17:30 -0300

    ATUALIZAÇÃO

--- formulario-cadastro-cliente/formulario.php
+++ formulario-cadastro-cliente/formulario.php
@@ -1,3 +1,38 @@
+<?php
+
+     if(isset($_POST['submit']))
+     {
+    //     print_r('Nome:' . $_POST['nome']);
+    //     print_r('<br>');
+    //     print_r('Email:' . $_POST['email']);
+    //     print_r('<br>');
+    //     print_r('telefone' . $_POST['telefone']);
+    //     print_r('<br>');
+    //     print_r('Sexo:' . $_POST['genero']);
+    //     print_r('<br>');
+    //     print_r('Data de nascimento:' . $_POST['data_nascimento']);
+    //     print_r('<br>');
+    //     print_r('Cidade' . $_POST['cidade']);
+    //     print_r('<br>');
+    //     print_r('Estado' . $_POST['estado']);
+    //     print_r('<br>');
+    //     print_r('Endereço' . $_POST['endereco']);
+        
+        include_once('config.php');
+
+        $nome = $_POST['nome'];
+        $email = $_POST['email'];
+        $telefone = $_POST['telefone'];
+        $sexo = $_POST['genero'];
+        $data_nasc =  $_POST['data_nascimento'];
+        $cidade = $_POST['cidade'];
+        $estado = $_POST['estado'];
+        $endereco = $_POST['endereco'];
+
+        $result = mysqli_query($conexao, "INSERT INTO usuarios(nome,email,telefone,sexo,data_nasc,cidade,estado,endereco)
+        VALUES ('$nome','$email','$telefone','$sexo','$data_nasc','$cidade','$estado','$endereco')");
+    }
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -9,7 +44,7 @@
 </head>
 <body>
     <div class="box">
-        <form action="">
+        <form action="formulario.php" method="POST">
             <fieldset>
                 <legend><b>Fórmulário de cadastro de clientes</b></legend>
                 <br>
